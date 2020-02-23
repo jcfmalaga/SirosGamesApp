af9d19ce7b9a1d2771ee0e12eb9456710d857f93
Juan Carlos <jcfmalaga@gmail.com>
2020-02-23 17:50:32 +0100

    Add project files.

---
+++ SirosGamesApp/SirosGamesApp/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace SirosGamesApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            AppCenter.Start("26f10be2-8008-4fdf-a795-02d75b3e22f1",
+                   typeof(Analytics), typeof(Crashes));
+
+            //MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SirosGamesApp/SirosGamesApp/SongsGame.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SirosGamesApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SongsGame : ContentPage
+    {
+        public SongsGame()
+        {
+            InitializeComponent();
+        }
+        public void AddCarlos_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            lblCarlos.Text = String.Format("{0}", e.NewValue);
+        }
+
+        public void AddJuanCarlos_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            lblJuanCarlos.Text = String.Format("{0}", e.NewValue);
+        }
+
+        public void AddNacho_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            lblNacho.Text = String.Format("{0}", e.NewValue);
+        }
+
+        public void AddJose_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            lblJose.Text = String.Format("{0}", e.NewValue);
+        }
+
+
+    }
+
+
+}
